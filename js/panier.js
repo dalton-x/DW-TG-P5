2176275425c6a29fa9cbd3017d319411d6c53aed
dalton-x <daltonx43@gmail.com>
2020-04-25 12:25:51 +0200

    start adding cart setup and checking empty cart

--- js/panier.js
+++ js/panier.js
@@ -1,10 +1,8 @@
 // variable pour affecter l'objet XMLHttpRequest
-let requestProduct = new XMLHttpRequest();
+let requestPanier = new XMLHttpRequest();
 
 // Recuperation des valeur dans l' URL
-//let IDproduct = $_GET('IDProduct');
-
-
+let indexColor = $_GET('color');
 
 // recuperation des cookies créés sur la/les pages produit(s)
 function getCookie(name){
@@ -24,91 +22,117 @@ function getCookie(name){
     return null;
 }
 
-// Récupération des cookies et splits en variables
-let getCookies = getCookie('cookies')
-const cookies = getCookies.split(',');
-let IDProduct = cookies[0];
-let color = cookies[1];
-let quantity = cookies[2];
+const eraseHtml = container => {
+    const el = document.getElementById(container);
+    while (el.firstChild) el.removeChild(el.firstChild);
+};
 
 // ouvre une nouvelle connexion en utilisant la méthode GET
-requestProduct.open('GET', urlDB + cookies[0]);
+requestPanier.open('GET', urlDB);
 
 // charge les données de notre JSON récupéré
-requestProduct.onload = function () {
+requestPanier.onload = function () {
 
     let teddiesChoice = JSON.parse(this.response);
 
-    // declaration des variables de la page html
-    let name = teddiesChoice.name;
-    let id = teddiesChoice._id;
-    let price = teddiesChoice.price;
-    let description = teddiesChoice.description;
-    let image = teddiesChoice.imageUrl;
+    for (let i = 0; i < teddiesChoice.length; i++) {
+    
+        // declaration des variables de la page html
+    let name = teddiesChoice[i].name;
+    let id = teddiesChoice[i]._id;
+    let price = teddiesChoice[i].price;
+    let color = teddiesChoice[i].colors;
+    let description = teddiesChoice[i].description;
+    let image = teddiesChoice[i].imageUrl;
 
-    // Mise en place du select du choix des couleurs de l'article
-    let imageProduct = document.getElementById("panier_image");
+    
+    }
+
+
+    var getAllCookies = document.cookie;
+    console.log(getAllCookies)
+    // Si panier vide on efface la page html et on remplace par le inner
+    if (getAllCookies == ""){
+        eraseHtml('inner')
+        var emptyPanier = document.getElementById("inner");
+        emptyPanier.innerHTML +=    '<div>'+
+                                       '<h2>Votre panier est vide</h2> '+
+                                       '<a href="../index.html">Retour a l\'acceuil</a>'+
+                                    '</div>';
+    }else{
+
+    
+        // Récupération des cookies et splits en variables
+        //let getCookies = getCookie(id+indexColor)
+        // const cookies = getCookies.split(',');
+        // let IDProduct = cookies[0];
+        // let colorCookies = cookies[1];
+        // let quantity = cookies[2];
+
+        }
+    // // Mise en place du select du choix des couleurs de l'article
+    // let imageProduct = document.getElementById("panier_image");
         
-    // ajout pour la balise image
-    const imgProduct = document.createElement("img");
-        imgProduct.src = image;
-        imgProduct.alt = description;
-        imgProduct.classList.add("grd-");
-        imgProduct.classList.add("image_product");
+    // // ajout pour la balise image
+    // const imgProduct = document.createElement("img");
+    //     imgProduct.src = image;
+    //     imgProduct.alt = description;
+    //     imgProduct.classList.add("grd-");
+    //     imgProduct.classList.add("image_product");
         
-        // Affichage de l'image
-        imageProduct.appendChild(imgProduct);
+    //     // Affichage de l'image
+    //     imageProduct.appendChild(imgProduct);
 
-    //Mise en place du titre en H2
-    let titleProduct = document.getElementById("panier_name");
-        const h2 = document.createElement("h3");
-        h2.textContent = name;
+    // //Mise en place du titre en H2
+    // let titleProduct = document.getElementById("panier_name");
+    //     const h2 = document.createElement("h3");
+    //     h2.textContent = name;
 
-        // Affichage du titre
-        titleProduct.appendChild(h2);
+    //     // Affichage du titre
+    //     titleProduct.appendChild(h2);
 
-    // Mise en place de la description de l article
-    let descProduct = document.getElementById("panier_description");
-        const pDesc = document.createElement("p");
-        pDesc.textContent = description;
+    // // Mise en place de la description de l article
+    // let descProduct = document.getElementById("panier_description");
+    //     const pDesc = document.createElement("p");
+    //     pDesc.textContent = description;
 
-        // Affichage de la description
-        descProduct.appendChild(pDesc);
+    //     // Affichage de la description
+    //     descProduct.appendChild(pDesc);
 
-    // Mise en place du prix de l'article à l'unitée
-    let priceProduct = document.getElementById("panier_price");
-        const pPrice = document.createElement("p");
-        pPrice.textContent = +price+" €";
+    // // Mise en place du prix de l'article à l'unitée
+    // let priceProduct = document.getElementById("panier_price");
+    //     const pPrice = document.createElement("p");
+    //     pPrice.textContent = +price+" €";
 
-        // Affichage du prix
-        priceProduct.appendChild(pPrice);
+    //     // Affichage du prix
+    //     priceProduct.appendChild(pPrice);
 
-    // Mise en place du prix de l'article à l'unitée
-    let quantityProduct = document.getElementById("panier_quantity");
-        const pQuantity = document.createElement("p");
-        pQuantity.textContent = quantity;
+    // // Mise en place du prix de l'article à l'unitée
+    // let quantityProduct = document.getElementById("panier_quantity");
+    //     const pQuantity = document.createElement("p");
+    //     pQuantity.textContent = quantity;
 
-        // Affichage du prix
-        quantityProduct.appendChild(pQuantity);
+    //     // Affichage du prix
+    //     quantityProduct.appendChild(pQuantity);
 
-    // Mise en place du prix de l'article à l'unitée
-    let colorProduct = document.getElementById("panier_color");
-        const pColor = document.createElement("p");
-        pColor.textContent = color;
+    // // Mise en place du prix de l'article à l'unitée
+    // let colorProduct = document.getElementById("panier_color");
+    //     const pColor = document.createElement("p");
+    //     pColor.textContent = color;
 
-        // Affichage du prix
-        colorProduct.appendChild(pColor);
+    //     // Affichage du prix
+    //     colorProduct.appendChild(pColor);
 
-    // Mise en place du prix de l'article à l'unitée
-    let subTotalProduct = document.getElementById("panier_subttotal");
-        const psubtotal = document.createElement("p");
-        psubtotal.textContent = teddiesChoice.price*quantity+" €";
+    // // Mise en place du prix de l'article à l'unitée
+    // let subTotalProduct = document.getElementById("panier_subttotal");
+    //     const psubtotal = document.createElement("p");
+    //     psubtotal.textContent = teddiesChoice.price*quantity+" €";
 
-        // Affichage du prix
-        subTotalProduct.appendChild(psubtotal);
+    //     // Affichage du prix
+    //     subTotalProduct.appendChild(psubtotal);
     
 
 }
 
 // on envoie la requête
-requestProduct.send()
+requestPanier.send()
