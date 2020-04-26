828a27e6ddace8c6968c27271b5d9eaeaef667a4
dalton-x <daltonx43@gmail.com>
2020-04-26 20:51:34 +0200

    ajout d'un fichier lang

---
+++ js/lang.js
@@ -0,0 +1,29 @@
+let language ={
+    fr : {
+        index : {
+            link : "produit.html?IDProduct=",
+            desc : "description du produit : ",
+            fiche : "Fiche produit",
+            price : "prix : "
+        },
+        produit : {
+            buttonTitle : "Validé l'article",
+            location : "./panier.html",
+            choiceColor : "Choix de la couleur :",
+            titleDescription : "Description de l'article",
+            quantity : "Quantitée :",
+            dontColor : "Vous n'avez pas choisit votre couleur",
+            openBigSize : "Cliquez ICI pour voir en plus grand"
+        },
+        panier : {
+            inner : '<div>'+'<h2>Votre panier est vide</h2> '+'<a href="../index.html">Retour a l\'acceuil</a>'+'</div>',
+            expiration : "= ; expires = Thu, 01 Jan 1970 00:00:00 GMT",
+            totalCommande : "total de votre commande ",
+            emptyCart : "Vider le panier",
+            priceUnit : "prix à l'unitée : ",
+            color : "Couleur : ",
+            quantity : "Quantitée : ",
+            subTotal : "Sous-Total : "
+        }
+    }
+}
